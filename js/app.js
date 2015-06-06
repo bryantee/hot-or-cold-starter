5a04ffd1afa49fd46080eebf0ec06692eb120329
Bryan Swagerty <bryantee@gmail.com>
2015-06-06 10:36:53 -0700

    Initial setup
    Setup newGame() function. Listener for on click ‘New Game’ link it
    calls newGame() function and generates a random number between 1 and
    100.

--- js/app.js
+++ js/app.js
@@ -12,6 +12,28 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*--- New Game ---*/
+  	$(".new").click(function() {
+  		console.log('New game starting!');
+  		newGame();
+  	});
+
+  	// Testing random number
+  	// remove before production
+	newGame();
+
 });
+
+var newGame = function(){
+	var randomNumber = setRandomNumber(1, 100);
+  	console.log('Random number is: ' + randomNumber + '\n Shhhhhhh....');
+}
+
+var setRandomNumber = function(min, max){
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+
 
 
