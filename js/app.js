c78211698627a75f6aa7f9bb2cbdab5efdbcaa7b
Bryan Swagerty <bryantee@gmail.com>
2015-06-06 14:41:08 -0700

    Early game logic functioning, feedback in console.
    Setup the basic functions. User makes guesses on page using the text
    input and button, but receives feedback in the console only at the
    moment.

--- js/app.js
+++ js/app.js
@@ -18,19 +18,51 @@ $(document).ready(function(){
   		newGame();
   	});
 
+  	/*-- Take Guess from Field on button click ---*/
+  	$("#guessButton").click(function() {
+  		var guess = $('#userGuess').val();
+  		var printResults = hotOrCold(compareGuess(guess, randomNumber));
+  		console.log(printResults);
+  	});
+
   	// Testing random number
   	// remove before production
 	newGame();
 
+
+
 });
 
+/*--- FUNCTIONS --*/
+
+// sets up game by calling setRandomNumber()
+// and printing to console
 var newGame = function(){
-	var randomNumber = setRandomNumber(1, 100);
+	randomNumber = setRandomNumber(1, 100);
   	console.log('Random number is: ' + randomNumber + '\n Shhhhhhh....');
 }
-
+// Sets the number between 1 and 100
 var setRandomNumber = function(min, max){
 	return Math.floor(Math.random() * (max - min) + min);
+}
+
+// finds the difference between the guess and the random number
+var compareGuess = function(guess, number){
+	var difference = Math.abs(guess - number);
+	return difference;
+}
+
+// takes the difference as argument, determines hot, cold or correct
+var hotOrCold = function(difference){
+	if (difference === 0) {
+		return 'Win!';
+	}
+	else if (difference <= 25) {
+		return 'Hot!';
+	}
+	else {
+		return 'Cold!';
+	}
 }
 
 
