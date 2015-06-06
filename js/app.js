3c419d34738568d24e8aafaa5af2ea83b2735e8c
Bryan Swagerty <bryantee@gmail.com>
2015-06-06 15:31:18 -0700

    Minimal game requirements working
    Next up: to add a few features (i.e. limit guesses, validate guess
    type, etc.) and customize layout.

--- js/app.js
+++ js/app.js
@@ -23,15 +23,24 @@ $(document).ready(function(){
   		var guess = $('#userGuess').val();
   		var printResults = hotOrCold(compareGuess(guess, randomNumber));
   		console.log(printResults);
+  		// give feedback in the #feedback area
+  		// but first clear the existing text
+  		$('h2#feedback').empty().append(printResults);
+  		$('ul#guessList').append('<li>' + guess + '</li>');
+  		// increase guess counter
+  		count += 1;
+  		$('#count').empty().append(count);
   	});
 
-  	// Testing random number
-  	// remove before production
 	newGame();
 
 
 
 });
+
+/*--- VARIABLES --*/
+
+var count = 0;
 
 /*--- FUNCTIONS --*/
 
@@ -57,8 +66,17 @@ var hotOrCold = function(difference){
 	if (difference === 0) {
 		return 'Win!';
 	}
+	else if (difference <= 5) {
+		return 'Blazin\'!';
+	}
 	else if (difference <= 25) {
 		return 'Hot!';
+	}
+	else if (difference <= 50) {
+		return 'Warm...';
+	}
+	else if (difference >= 51) {
+		return 'You\'re as cold as ice...';
 	}
 	else {
 		return 'Cold!';
