84accb391fd8b893fca991e608445d7416bc0ec3
Bryan Swagerty <bryantee@gmail.com>
2015-06-07 10:38:17 -0700

    Relative feedback added based on current and last guess
    Still need to refactor some code into functions. But working and don’t
    want to mess with yet.

--- js/app.js
+++ js/app.js
@@ -27,20 +27,29 @@ $(document).ready(function(){
   		// but first clear the existing text
   		$('h2#feedback').empty().append(printResults);
   		$('ul#guessList').append('<li>' + guess + '</li>');
-  		// increase guess counter
+  		// check count, if greater than 0,
+      // run this second hotOrCold function
+      if (count >= 1) {
+        var printAdditionalFeedback = hotOrCold2(compareGuess(guess, randomNumber));
+        console.log(printAdditionalFeedback);
+        $('h2#feedback').append(' | ' + printAdditionalFeedback);
+      };
+      // increase guess counter
   		count += 1;
   		$('#count').empty().append(count);
+      // set seperate global variable to keep track of last guess
+      lastGuess = guess;
+
   	});
 
 	newGame();
-
-
 
 });
 
 /*--- VARIABLES --*/
 
 var count = 0;
+var lastGuess;
 
 /*--- FUNCTIONS --*/
 
@@ -56,6 +65,8 @@ var newGame = function(){
   	$('#count').empty().append('0');
   	// change #feedback back to 'Make your guess!'
   	$('h2#feedback').empty().append('Make Your Guess!');
+    // reset input place holder
+    $('#userGuess').val('');
 
 }
 // Sets the number between 1 and 100
@@ -90,6 +101,26 @@ var hotOrCold = function(difference){
 		return 'Cold!';
 	}
 }
+
+// alternate take on the logic
+// use for guesses after the first guess
+// compares current guess to last guess for relative feedback
+var hotOrCold2 = function(difference){
+  var lastDifference = compareGuess(lastGuess, randomNumber);
+  var currentDifference = difference;
+  // for testing
+  console.log('Current Difference: ' + currentDifference);
+  console.log('Last Difference: ' + lastDifference);
+  if (difference === 0) {
+    return 'Good Job!';
+  }
+  else if (currentDifference <= lastDifference) {
+    return 'Getting warmer...';
+  }
+  else {
+    return 'Getting colder...';
+  }
+};
 
 
 
