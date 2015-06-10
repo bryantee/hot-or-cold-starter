865f424e6da790b52600522c122c8433035ab044
bryantee <bryantee@gmail.com>
2015-06-10 08:33:50 -0700

    Refactor code to more efficient functions. Added sanitize function for guess input.

--- js/app.js
+++ js/app.js
@@ -23,22 +23,13 @@ $(document).ready(function(){
     // ran into issues with variable scope when tried so left for now
   	$("#guessButton").click(function() {
   		var guess = $('#userGuess').val();
-  		var printResults = hotOrCold(compareGuess(guess, randomNumber));
-  		console.log(printResults);
-  		// give feedback in the #feedback area
-  		// but first clear the existing text
-  		$('h2#feedback').empty().append(printResults);
-  		$('ul#guessList').append('<li>' + guess + '</li>');
-  		// check count, if greater than 0,
-      // run this second hotOrCold function
-      if (count >= 1) {
-        var printAdditionalFeedback = hotOrCold2(compareGuess(guess, randomNumber));
-        console.log(printAdditionalFeedback);
-        $('h2#feedback').append(' | ' + printAdditionalFeedback);
-      };
-  		count += 1; // increment guess counter
-  		$('#count').empty().append(count); //increment counter on page
-      lastGuess = guess; // update global variable to keep track of the last guess
+      if (sanitizeGuess(guess)) {
+    		hotOrCold(compareGuess(guess, randomNumber));
+    		$('ul#guessList').append('<li>' + guess + '</li>');
+    		count += 1; // increment guess counter
+    		$('#count').empty().append(count); //increment counter on page
+        lastGuess = guess; // update global variable to keep track of the last guess
+      }
 
   	});
 
@@ -46,6 +37,10 @@ $(document).ready(function(){
 	newGame();
 
 });
+
+/*--- END OF DOCUMENT LOAD FUNCTION ---*/
+
+/////////////////////////////////////////
 
 /*--- VARIABLES --*/
 
@@ -77,49 +72,75 @@ var compareGuess = function(guess, number){
 	return difference;
 }
 
-// takes the difference as argument, determines hot, cold or correct
-var hotOrCold = function(difference){
-	if (difference === 0) {
-		return 'Win!';
-	}
-	else if (difference <= 5) {
-		return 'Blazin\'!';
-	}
-	else if (difference <= 25) {
-		return 'Hot!';
-	}
-	else if (difference <= 50) {
-		return 'Warm...';
-	}
-	else if (difference >= 51) {
-		return 'You\'re as cold as ice...';
-	}
-	else {
-		return 'Cold!';
-	}
-}
-
-// alternate take on the logic
-// use for guesses after the first guess
-// compares current guess to last guess for relative feedback
-var hotOrCold2 = function(difference){
-  var lastDifference = compareGuess(lastGuess, randomNumber);
-  var currentDifference = difference;
-  
-  /*--- FOR TESTING ---*/
-  //console.log('Current Difference: ' + currentDifference);
-  //console.log('Last Difference: ' + lastDifference);
-  
-  if (difference === 0) {
-    return 'Good Job!';
+var sanitizeGuess = function(guess) {
+  if (guess === '') {
+    alert("This is blank");
+    return false;
   }
-  else if (currentDifference <= lastDifference) {
-    return 'Getting warmer...';
+  else if (isNaN(parseInt(guess))){
+    alert("Not a number");
+    return false;
+  }
+  else if (guess <= 0){ 
+    return false;
+  }
+  else if (guess >= 100) {
+    return false;
   }
   else {
-    return 'Getting colder...';
+    return true;
   }
-};
+}
+
+// takes the difference as argument, determines hot, cold or correct
+var hotOrCold = function(difference){
+  var output = ''
+  console.log(output);
+
+	if (difference === 0) {
+		//return 'Win!';
+    output += 'Win!';
+	}
+	else if (difference <= 5) {
+		//return 'Blazin\'!';
+    output += 'Blazin';
+	}
+	else if (difference <= 25) {
+		output += 'Hot!';
+
+	}
+	else if (difference <= 50) {
+		output += 'Warm...';
+	}
+	else if (difference >= 51) {
+		output += 'You\'re as cold as ice...';
+	}
+	else {
+		output += 'Cold!';
+	}
+  console.log(output);
+
+  if (count >= 1) {
+    var lastDifference = compareGuess(lastGuess, randomNumber);
+    var currentDifference = difference;
+    
+    /*--- FOR TESTING ---*/
+    //console.log('Current Difference: ' + currentDifference);
+    //console.log('Last Difference: ' + lastDifference);
+    output += ' | ';
+
+    if (difference === 0) {
+      output += 'Good Job!';
+    }
+    else if (currentDifference <= lastDifference) {
+      output += 'Getting warmer...';
+    }
+    else {
+      output += 'Getting colder...';
+    }
+  }
+$('h2#feedback').empty().append(output);
+}
 
 
 
